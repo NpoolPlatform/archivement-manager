36979ed422998f99df1e3ffed673487eca3c7525
Zhao KK <kikakkz@hotmail.com>
2022-07-27 08:23:43 +0000

    Move commission and archivement to middleware

--- api/api.go
+++ api/api.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/NpoolPlatform/message/npool/archivementmgr"
 
-	"github.com/NpoolPlatform/archivement-manager/api/archivement"
-	"github.com/NpoolPlatform/archivement-manager/api/commission"
 	"github.com/NpoolPlatform/archivement-manager/api/detail"
 	"github.com/NpoolPlatform/archivement-manager/api/general"
 
@@ -20,20 +18,12 @@ type Server struct {
 
 func Register(server grpc.ServiceRegistrar) {
 	archivementmgr.RegisterArchivementManagerServer(server, &Server{})
-	commission.Register(server)
-	archivement.Register(server)
 	detail.Register(server)
 	general.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
 	if err := archivementmgr.RegisterArchivementManagerHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
-		return err
-	}
-	if err := commission.RegisterGateway(mux, endpoint, opts); err != nil {
-		return err
-	}
-	if err := archivement.RegisterGateway(mux, endpoint, opts); err != nil {
 		return err
 	}
 	if err := detail.RegisterGateway(mux, endpoint, opts); err != nil {
